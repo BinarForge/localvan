e913a0b50425e58706277c68a28fb9d3b075b4f0
BinarForge <contact@binarforge.com>
2017-03-23 19:53:22 +0100

    Moved the header into the app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'localvan-app',
-  template: 'Hello LocalVan!'
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent {}
